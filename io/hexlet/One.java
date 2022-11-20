5ad41f226ae269c1152e8f22b15ce68724809c13
melnikowww <melnikoffspb@ya.ru>
2022-11-20 21:07:08 +0300

    тут много всего...

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,10 @@
 package io.hexlet;
+import org.apache.commons.lang3.StringUtils;
 
 public class One {
 public static void main(String[] args) {
+String reverse = StringUtils.reverse("Hexlet");
+System.out.println(reverse);
 Two.greeting();
 }
 }
